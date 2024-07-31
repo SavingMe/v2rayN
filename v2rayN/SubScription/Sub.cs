45b60925e7f6bd2cd16a1624d1a0c8305857d85b
326500805 <326500805@qq.com>
2024-07-31 21:16:47 +0800

    init

---
+++ v2rayN/SubScription/Sub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SubScription
+{
+    public class Sub
+    {
+        public static string GetAndPrintIPv6Addresses(string netInterfaceName = "ETH0", string perfix = "2409")
+        {
+            Console.WriteLine($"Fetching IP addresses at {DateTime.Now}:");
+
+            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // 检查网络接口是否处于活动状态
+                if (netInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    // 获取所有的IP地址信息
+                    IPInterfaceProperties properties = netInterface.GetIPProperties();
+                    foreach (UnicastIPAddressInformation ipInfo in properties.UnicastAddresses)
+                    {
+                        // 筛选出IPv6地址
+                        if (ipInfo.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                        {
+                            Console.WriteLine($"Interface {netInterface.Name}: {ipInfo.Address}");
+                            if (netInterface.Name.Contains(netInterfaceName) && ipInfo.Address.ToString().StartsWith(perfix))
+                            {
+                                return ipInfo.Address.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
--- v2rayN/v2rayN/Views/MainWindow.xaml.cs
+++ v2rayN/v2rayN/Views/MainWindow.xaml.cs
@@ -12,16 +12,37 @@ using v2rayN.Handler;
 using v2rayN.Models;
 using v2rayN.Resx;
 using v2rayN.ViewModels;
-
+using System.Runtime.Intrinsics.Arm;
+using YamlDotNet.RepresentationModel;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 namespace v2rayN.Views
 {
     public partial class MainWindow
     {
         private static Config _config;
-
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+            #region 自定义
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(5000);
+                    string ipv6 = GetAndPrintIPv6Addresses();
+                    Dictionary<string, string> headers = new Dictionary<string, string>();
+                    headers.Add("all", ipv6);
+                    var aa = HttpClientHelper.Instance.PutAsync("http://localhost:5005/home/putall", headers);
+                }
+            }, TaskCreationOptions.LongRunning);
+
+            #endregion
+
+
 
             _config = LazyConfig.Instance.GetConfig();
 
@@ -342,5 +363,33 @@ namespace v2rayN.Views
         }
 
         #endregion UI
+
+        private static string GetAndPrintIPv6Addresses(string netInterfaceName = "ETH0", string perfix = "2409")
+        {
+            Console.WriteLine($"Fetching IP addresses at {DateTime.Now}:");
+
+            foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // 检查网络接口是否处于活动状态
+                if (netInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    // 获取所有的IP地址信息
+                    IPInterfaceProperties properties = netInterface.GetIPProperties();
+                    foreach (UnicastIPAddressInformation ipInfo in properties.UnicastAddresses)
+                    {
+                        // 筛选出IPv6地址
+                        if (ipInfo.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                        {
+                            Console.WriteLine($"Interface {netInterface.Name}: {ipInfo.Address}");
+                            if (netInterface.Name.Contains(netInterfaceName) && ipInfo.Address.ToString().StartsWith(perfix))
+                            {
+                                return ipInfo.Address.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
